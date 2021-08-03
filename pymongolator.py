be25dd9820d6827ae3e80456bf9a3c749fe1c536
Souvik Roy <50344035+souvikroy93@users.noreply.github.com>
2021-08-03 15:16:40 +0200

    Update pymongolator.py

--- pymongolator.py
+++ pymongolator.py
@@ -1,5 +1,5 @@
-import proj_pyMongolator.main
-from proj_pyMongolator.main import WelcomeWindow
+import src.main
+from src.main import WelcomeWindow
 import sys
 from PyQt5.QtWidgets import QApplication
 
@@ -8,6 +8,3 @@ if __name__ == '__main__':
     win = WelcomeWindow()
     win.show()
     sys.exit(app.exec_())
-
-
-
